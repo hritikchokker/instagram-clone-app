609d6670c17ed3106940c21c26b3ade6361da529
dograkunal <kunaldogra31@gmail.com>
2022-05-07 21:03:33 +0530

    Login ui

---
+++ apps/frontend/src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss';
+import logoImg from '../../assets/pro-max.png';
+
+function Login() {
+  return (
+    <div className="Login_base">
+      <div>
+        <img src={logoImg} alt="login image" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Login;
--- apps/frontend/src/components/MainBody/index.js
+++ apps/frontend/src/components/MainBody/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './index.scss';
 import MainContainer from '../MainContainer/index';
 import ProfileContainer from '../ProfileContainer/index';
+import Login from '../Login/Login';
 function Index() {
   return (
-    <div className="body_base">
-      <div className="body_base_container">
-        <MainContainer />
-        <ProfileContainer />
-      </div>
-    </div>
+    // <div className="body_base">
+    //   <div className="body_base_container">
+    //     <MainContainer />
+    //     <ProfileContainer />
+    //   </div>
+    // </div>
+    <>
+      <Login />
+    </>
   );
 }
 
